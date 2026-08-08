5b0276bae5c148f88cc0a761e0a73c86db159b2c
kshshrma <ks2012150@gmail.com>
2026-08-08 10:57:05 +0530

    Add user profile management

--- client/src/pages/Profile.tsx
+++ client/src/pages/Profile.tsx
@@ -2,7 +2,7 @@ import ProfileForm from "../features/profile/ProfileForm";
 
 export default function Profile() {
   return (
-    <div className="min-h-screen bg-slate-100 px-5 py-8 pb-28">
+    <div className="min-h-screen bg-slate-100 p-5 pb-28">
       <ProfileForm />
     </div>
   );
