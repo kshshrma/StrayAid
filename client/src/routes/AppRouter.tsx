00f3dd92ec666cfae18d8411bccad667c62082de
kshshrma <ks2012150@gmail.com>
2026-08-08 23:05:10 +0530

    guardian created

--- client/src/routes/AppRouter.tsx
+++ client/src/routes/AppRouter.tsx
@@ -8,26 +8,47 @@ import ReportPage from "../features/report/ReportPage";
 import Community from "../pages/Community";
 import Profile from "../pages/Profile";
 import TestConnection from "../pages/TestConnection";
+
 import Register from "../features/auth/Register";
 import Login from "../features/auth/Login";
+
 import ReportsFeed from "../features/report/ReportsFeed";
 import ReportDetails from "../features/report/ReportDetails";
+
+import GuardianForm from "../features/guardian/GuardianForm";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
+
         <Route path="/map" element={<Map />} />
+
         <Route path="/report" element={<ReportPage />} />
+
         <Route path="/community" element={<Community />} />
+
         <Route path="/profile" element={<Profile />} />
+
         <Route path="/test" element={<TestConnection />} />
+
         <Route path="/register" element={<Register />} />
+
         <Route path="/login" element={<Login />} />
+
         <Route path="/reports" element={<ReportsFeed />} />
-        <Route path="/reports/:id" element={<ReportDetails />} />
-        {/* <Route path="/guardian" element={<Guardian />} /> */}
+
+        <Route
+          path="/reports/:id"
+          element={<ReportDetails />}
+        />
+
+        {/* Guardian Registration */}
+        <Route
+          path="/guardian"
+          element={<GuardianForm />}
+        />
       </Routes>
 
       <BottomNav />
