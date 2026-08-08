7eaf998772ae43cdc7bce1c4a3215fde6ffbaacf
kshshrma <ks2012150@gmail.com>
2026-08-08 10:39:25 +0530

    added updation for profile

---
+++ client/src/services/profile/getProfile.ts
@@ -0,0 +1,15 @@
+import { supabase } from "../../lib/supabase";
+
+export async function getProfile(userId: string) {
+  const { data, error } = await supabase
+    .from("profiles")
+    .select("*")
+    .eq("id", userId)
+    .maybeSingle();
+
+  if (error) {
+    throw error;
+  }
+
+  return data;
+}
