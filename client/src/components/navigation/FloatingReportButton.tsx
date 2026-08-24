09db4903a55ab2f9783b248059439a21d4ebd454
kshshrma <ks2012150@gmail.com>
2026-08-24 22:50:34 +0530

    Update FloatingReportButton layout to pill shape with text 'Report Animal'

--- client/src/components/navigation/FloatingReportButton.tsx
+++ client/src/components/navigation/FloatingReportButton.tsx
@@ -13,20 +13,25 @@ export default function FloatingReportButton() {
       left-1/2
       z-50
       flex
-      h-16
-      w-16
+      h-14
+      px-6
       -translate-x-1/2
       items-center
       justify-center
+      gap-2
       rounded-full
       bg-green-700
       text-white
       shadow-xl
       transition
       hover:scale-105
+      font-semibold
+      text-sm
+      whitespace-nowrap
       "
     >
-      <Plus size={30} />
+      <Plus size={20} />
+      <span>Report Animal</span>
     </button>
   );
 }
